47817dd86afeb4990d0f309e716194c80e4155d4
GeorgeCiachir <ciachirg@ebrd.com>
2022-03-01 07:47:45 +0200

    Az-204 fix line separators for powershell

--- az-related/vm-deployments/CreateVMAzCLI.sh
+++ az-related/vm-deployments/CreateVMAzCLI.sh
@@ -8,28 +8,28 @@ az account set --subscription "TestAccount"
 az group list --output table
 
 # Create a resource group if needed
-az group create \
-    --name "implementing-IaaS-RG" \
+az group create `
+    --name "implementing-IaaS-RG" `
     --location "westeurope"
 
 #Creating a Windows VM
-az vm create \
-  --resource-group "implementing-IaaS-RG" \
-  --name "WinVM" \
-  --image "win2019datacenter" \
-  --admin-username "demoadmin" \
+az vm create `
+  --resource-group "implementing-IaaS-RG" `
+  --name "WinVM" `
+  --image "win2019datacenter" `
+  --admin-username "demoadmin" `
   --admin-password "Password123!"
 
 #Open RDP for remote access
-az vm open-port \
-  --resource-group "implementing-IaaS-RG" \
-  --name "WinVM" \
+az vm open-port `
+  --resource-group "implementing-IaaS-RG" `
+  --name "WinVM" `
   --port "3389"
 
 #Get the IP addresses for Remote Access
-az vm list-ip-addresses \
-  --resource-group "implementing-IaaS-RG" \
-  --name "WinVM" \
+az vm list-ip-addresses `
+  --resource-group "implementing-IaaS-RG" `
+  --name "WinVM" `
   --output table
 
 
@@ -43,31 +43,31 @@ az account set --subscription "TestAccount"
 az group list --output table
 
 # Create a resource group if needed
-az create \
-    --name "implementing-IaaS-RG" \
+az create `
+    --name "implementing-IaaS-RG" `
     --location "westeurope"
 
 #Generate ssh public key if none existing
 
 #Creating a Windows VM
-az vm create \
-  --resource-group "implementing-IaaS-RG" \
-  --name "LinuxVM" \
-  --image "UbuntuLTS" \
-  --admin-username "demoadmin" \
-  --authentication-type "ssh" \
+az vm create `
+  --resource-group "implementing-IaaS-RG" `
+  --name "LinuxVM" `
+  --image "UbuntuLTS" `
+  --admin-username "demoadmin" `
+  --authentication-type "ssh" `
   --ssh-key-value ~/.ssh/id_rsa.pub
 
 #Open RDP for remote access
-az vm open-port \
-  --resource-group "implementing-IaaS-RG" \
-  --name "LinuxVM" \
+az vm open-port `
+  --resource-group "implementing-IaaS-RG" `
+  --name "LinuxVM" `
   --port "22"
 
 #Get the IP addresses for Remote Access
-az vm list-ip-addresses \
-  --resource-group "implementing-IaaS-RG" \
-  --name "LinuxVM" \
+az vm list-ip-addresses `
+  --resource-group "implementing-IaaS-RG" `
+  --name "LinuxVM" `
   --output table
 
 #Log into the Linux VM
